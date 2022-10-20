d976ef0b7ed73d3db417970225c4cce57a2b7e4e
Imran Baali <ibaali@e1r6p5.1337.ma>
2022-10-20 12:11:06 +0100

    add notify messages

--- code.js
+++ code.js
@@ -1,64 +1,64 @@
-// This file holds the main code for the plugin. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
-// Runs this code if the plugin is run in Figma
-
 const createdStyles = [];
+
+let notificationHandler;
 
 let count = 0;
 
-run().then (() => {
+run().then(() => {
 	figma.closePlugin();
-	console.log(`${count} styles created`);
+
+	notificationHandler.cancel();
+
+	notificationHandler = figma.notify(`${count} text styles created`, {
+		timeout: 2000,
+	});
 });
 
-function printTextNode(textNode) {
-	console.log(`characters => ${textNode.characters}`);
-	console.log(`fontSize => ${textNode.fontSize}`);
-	console.log(`fontName family => ${textNode.fontName.family} | style => ${textNode.fontName.style}`);
-	console.log(`letterSpacing value => ${textNode.letterSpacing.value} | unit => ${textNode.letterSpacing.unit}`);
-	console.log(`lineHeight value => ${textNode.lineHeight.value} | unit => ${textNode.lineHeight.unit}`);
-	console.log(`textStyleId => |${textNode.textStyleId}| {<==>} |${textNode.textStyleId === ""}|`);
+function showLoadingNotification(message) {
+	notificationHandler = figma.notify(message, {
+		timeout: 60000,
+	});
 }
 
-
-async function run () {
+async function run() {
+	notificationHandler = figma.notify("Creating text styles...", {
+		timeout: 60000,
+	});
 	if (figma.editorType === "figma") {
 		const textNodes = figma.root.findAll((node) => node.type === "TEXT");
-		
+
 		for (let i = 0; i < textNodes.length; i++) {
-			console.log("textNode => " + i);
-			if (textNodes[i].textStyleId.length > 0)
-				continue ;
-	
+			if (textNodes[i].textStyleId.length > 0) continue;
+
 			let isStyleCreated = false;
-			// check if the text style is already created
 			for (let j = 0; j < createdStyles.length; j++) {
-				console.log("Cheking..!!!");
 				if (
-					textNodes[i].fontName.family === createdStyles[j].fontNameFamily			&&
-					textNodes[i].fontName.style === createdStyles[j].fontNameStyle 				&&
-					textNodes[i].fontSize === createdStyles[j].fontSize							&&
-					textNodes[i].letterSpacing.value === createdStyles[j].letterSpacingValue	&&
-					textNodes[i].letterSpacing.unit === createdStyles[j].letterSpacingUnit		&&
-					textNodes[i].lineHeight.value === createdStyles[j].lineHeightValue			&&
-					textNodes[i].lineHeight.unit === createdStyles[j].lineHeightUnit
+					textNodes[i].fontName.family ===
+						createdStyles[j].fontNameFamily &&
+					textNodes[i].fontName.style ===
+						createdStyles[j].fontNameStyle &&
+					textNodes[i].fontSize === createdStyles[j].fontSize &&
+					textNodes[i].letterSpacing.value ===
+						createdStyles[j].letterSpacingValue &&
+					textNodes[i].letterSpacing.unit ===
+						createdStyles[j].letterSpacingUnit &&
+					textNodes[i].lineHeight.value ===
+						createdStyles[j].lineHeightValue &&
+					textNodes[i].lineHeight.unit ===
+						createdStyles[j].lineHeightUnit
 				) {
 					isStyleCreated = true;
 					textNodes[i].textStyleId = createdStyles[j].id;
-					console.log("style already created");
 					break;
 				}
 			}
-	
+
 			if (isStyleCreated == false) {
 				try {
 					await createNewTextStyle(textNodes[i]);
 					count += 1;
-					console.log("Error: Outside createNewTextStyle => " + err);
-				}
-				catch (err) {
-					console.log(err);
+				} catch (err) {
+					console.error(err);
 				}
 			}
 		}
@@ -67,19 +67,22 @@ async function run () {
 
 async function createNewTextStyle(textNode) {
 	try {
-		await figma.loadFontAsync(textNode.fontName)
+		await figma.loadFontAsync(textNode.fontName);
 		const newStyle = figma.createTextStyle();
 		newStyle.fontName = textNode.fontName;
 		newStyle.fontSize = textNode.fontSize;
 		newStyle.letterSpacing = textNode.letterSpacing;
 		newStyle.lineHeight = textNode.lineHeight;
 		newStyle.lineHeight = textNode.lineHeight;
-		newStyle.name = textNode.fontName.family + "_" + textNode.fontName.style + "_" + textNode.fontSize;
-	
+		newStyle.name =
+			textNode.fontName.family +
+			"_" +
+			textNode.fontName.style +
+			"_" +
+			textNode.fontSize;
+
 		textNode.textStyleId = newStyle.id;
-		
-		console.log("style created successfully");
-	
+
 		createdStyles.push({
 			fontSize: textNode.fontSize,
 			fontNameFamily: textNode.fontName.family,
@@ -90,10 +93,7 @@ async function createNewTextStyle(textNode) {
 			lineHeightUnit: textNode.lineHeight.unit,
 			id: newStyle.id,
 		});
-		console.log(createdStyles);
+	} catch (err) {
+		console.error(err);
 	}
-	catch (err) {
-		console.log("Error: Inside createNewTextStyle => " + err);
-	}
-
 }
